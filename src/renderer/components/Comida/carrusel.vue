7619aa44957128bc10e5d3abe9e685fbf3ab637b
guillermoLeon26 <31398169+guillermoLeon26@users.noreply.github.com>
2018-07-15 16:45:11 -0500

    laravel echo falta

--- src/renderer/components/Comida/carrusel.vue
+++ src/renderer/components/Comida/carrusel.vue
@@ -3,48 +3,14 @@
     <div class="col">
       <div id="carouselControls" class="carousel slide" data-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="d-block w-100" src="static/1.jpg" alt="First slide">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Almuerzo</h5>
-              <p>$3.25</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="static/2jpg.jpg" alt="Second slide">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Merienda</h5>
-              <p>$2.50</p>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="d-block w-100" src="static/3.jpg" alt="Third slide">
-            <div class="carousel-caption d-none d-md-block">
-              <h5>Desayuno</h5>
-              <p>$2.00</p>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselControls" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-    </div>
-
-    <div class="col">
-      <div id="carouselControls" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
           <imagen 
             v-for="(comida, index) in comidas"
             :url="comida.url"
             :nombre="comida.nombre"
             :precio="comida.precio"
-          ></imagen>
+            :isActive="index == 0"
+          >
+          </imagen>
         </div>
         <a class="carousel-control-prev" href="#carouselControls" role="button" data-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,15 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import Echo from 'laravel-echo'
+
+window.Echo = new Echo({
+  broadcaster: 'pusher',
+  key: '1a7c234fb4766066b053',
+  cluster: 'us2',
+  encrypted: false
+})
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
